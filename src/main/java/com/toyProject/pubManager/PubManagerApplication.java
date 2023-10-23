c9c6b149ac16748865a8c4456888d572bba9d607
Robert <robert@Robertui-MacBookPro.local>
2023-10-23 23:26:54 +0900

    init

---
+++ src/main/java/com/toyProject/pubManager/PubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.toyProject.pubManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyProject/pubManager/PubManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyProject.pubManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
