8b3d2d1dabf18120fdd9e12572db2c4c155074c8
Robert <robert@Robertui-MacBookPro.local>
2023-11-07 00:45:58 +0900

    설정 진행 중

---
+++ src/main/java/com/toyProject/pubManager/model/service/MenuInfoService.java
@@ -0,0 +1,14 @@
+package com.toyProject.pubManager.model.service;
+
+public class MenuInfoService {
+
+    public String testService(){
+
+
+        System.out.println();
+
+        return null;
+    }
+
+
+}
